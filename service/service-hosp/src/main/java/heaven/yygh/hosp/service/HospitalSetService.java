95c4e78eeb6fe6e90b4d2d5623b06a739d9b5932
Heaven <Heaven@hai.com>
2022-04-10 10:52:41 +0800

    医院上传接口完成

--- service/service-hosp/src/main/java/heaven/yygh/hosp/service/HospitalSetService.java
+++ service/service-hosp/src/main/java/heaven/yygh/hosp/service/HospitalSetService.java
@@ -2,6 +2,7 @@ package heaven.yygh.hosp.service;
 
 
 import com.baomidou.mybatisplus.extension.service.IService;
+import heaven.yygh.model.hosp.Hospital;
 import heaven.yygh.model.hosp.HospitalSet;
 
 /**
@@ -11,5 +12,7 @@ import heaven.yygh.model.hosp.HospitalSet;
  * @Modified By:
  */
 public interface HospitalSetService extends IService<HospitalSet> {
+    //2、根据传递过来的医院编码，查询数据库，查询签名
+    String getSignKey(String hoscode);
 
 }
--- service/service-hosp/src/main/java/heaven/yygh/hosp/service/impl/HospitalSetServiceImpl.java
+++ service/service-hosp/src/main/java/heaven/yygh/hosp/service/impl/HospitalSetServiceImpl.java
@@ -1,5 +1,6 @@
 package heaven.yygh.hosp.service.impl;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import heaven.yygh.hosp.mapper.HospitalSetMapper;
 import heaven.yygh.hosp.service.HospitalSetService;
@@ -19,4 +20,12 @@ public class HospitalSetServiceImpl extends ServiceImpl<HospitalSetMapper, Hospi
         implements HospitalSetService {
     @Autowired
     private HospitalSetMapper hospitalSetMapper;
+
+    @Override
+    public String getSignKey(String hoscode) {
+        QueryWrapper<HospitalSet> wrapper=new QueryWrapper<>();
+        wrapper.eq("hoscode",hoscode);
+        HospitalSet hospitalSet = baseMapper.selectOne(wrapper);
+        return hospitalSet.getSignKey();
+    }
 }
